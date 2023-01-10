0815ac3a86ee4fe554135db1fd2ca7af4b64418d
Jatin Undhad <jatinundhad33@gmail.com>
2023-01-10 21:42:22 +0530

    third commit

--- app.js
+++ app.js
@@ -120,6 +120,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-app.listen(3000, () => {
-  console.log("Hi i am hearing here at port 3000");
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Hi i am hearing here at port 3000 ${port}`);
 });
